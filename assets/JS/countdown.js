a43ed4cbb44c823aac93559e3a329094cce751ce
FunomeHerbie <erickfunome8@gmail.com>
2024-10-29 20:57:54 +0700

    eddited

--- assets/JS/countdown.js
+++ assets/JS/countdown.js
@@ -1,15 +1,15 @@
- // Set the countdown date to 5 November 2024 at 18:00
+ // atur hitungan mundur ke 5 November 2024 pukul 18:00
  var target_date = new Date('November 5, 2024 18:00:00').getTime();
- var days, hours, minutes, seconds; // variables for time units
+ var days, hours, minutes, seconds; 
 
- var countdown = document.getElementById("tiles"); // get tag element
+ var countdown = document.getElementById("tiles"); 
 
  getCountdown();
 
  setInterval(function () { getCountdown(); }, 1000);
 
  function getCountdown() {
-     // find the amount of "seconds" between now and target
+     
      var current_date = new Date().getTime();
      var seconds_left = (target_date - current_date) / 1000;
 
@@ -22,7 +22,7 @@
      minutes = pad(parseInt(seconds_left / 60));
      seconds = pad(parseInt(seconds_left % 60));
 
-     // format countdown string + set tag value
+     
      countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
  }
 
